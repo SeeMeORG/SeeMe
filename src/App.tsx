6fb373a084a14ad6f77583423810acadff1ff94d
sky1412001 <akashjaggi1123@gmail.com>
2025-04-04 22:50:52 +0530

    #2: Name Change

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ export const App = () => {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vishal + Akash</h1>
       <div className="card">
         <Button variant='contained' onClick={() => setCount((count) => count + 1)}>
           count is {count}
