ee2cc16bdb57e677ce5efafc66036240b59597e0
Vishal Kumar <vishal878787k@gmail.com>
2025-04-06 22:34:02 +0530

    welcome page fixes

--- src/components/main/Welcome.tsx
+++ src/components/main/Welcome.tsx
@@ -1,38 +1,110 @@
-import { Dialog, Typography } from "@mui/material";
+import { Avatar, Box, Dialog } from "@mui/material";
 import { useEffect, useState } from "react";
+import logo from "../../assets/LOGO.png";
+import mannu from "../../assets/MannuGit.jpg";
+import vishal from "../../assets/VishalGit.jpg";
 import { WelcomeContainer } from "../../style/CommonStyles";
 import { muiTheme } from "../../style/muiTheme";
 import { IWelcomeProps } from "../interface";
 
 export const Welcome = ({ setWelcome }: IWelcomeProps) => {
-  const [count, setCount] = useState(5);
+  const [animate, setAnimate] = useState(false);
 
   useEffect(() => {
     setTimeout(() => {
-      setCount(count - 1);
-      if (count == 0) {
-        setWelcome(false);
-      }
-    }, 500);
+      setAnimate(true);
+    }, 1000);
+  }, []);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setWelcome(false);
+    }, 3000);
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [count]);
+  }, []);
 
   return (
     <Dialog open fullScreen>
       <WelcomeContainer
         sx={{ background: muiTheme.palette.background.default }}
       >
-        <Typography
-          variant="h4"
-          color="primary.dark"
-          fontWeight="bold"
-          textAlign="center"
+        <Box
           sx={{
-            textShadow: `0 0 5px ${muiTheme.palette.text.secondary}`,
+            position: "relative",
+            width: "100%",
+            height: "100vh",
+            overflow: "hidden",
+            backgroundColor: "transparent",
           }}
         >
-          Welcome Getting ready in {count}
-        </Typography>
+          {/* Left or Top Avatar */}
+          <Box
+            sx={{
+              position: "absolute",
+              top: "50%",
+              left: "50%",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              transition: "transform 1s ease-in-out",
+              transform: {
+                xs: animate
+                  ? "translate(-50%, -250%)"
+                  : "translate(-50%, -100%)", // vertical movement
+                md: animate
+                  ? "translate(-600%, -50%)"
+                  : "translate(-100%, -50%)", // horizontal movement
+              },
+            }}
+          >
+            <Avatar
+              src={mannu}
+              alt="Mannu Jaggi"
+              sx={{ width: 70, height: 70 }}
+            />
+          </Box>
+
+          {/* Right or Bottom Avatar */}
+          <Box
+            sx={{
+              position: "absolute",
+              top: "50%",
+              left: "50%",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              transition: "transform 1s ease-in-out",
+              transform: {
+                xs: animate ? "translate(-50%, 150%)" : "translate(-50%, 0%)",
+                md: animate ? "translate(600%, -50%)" : "translate(0%, -50%)",
+              },
+            }}
+          >
+            <Avatar
+              src={vishal}
+              alt="Vishal Kumar"
+              sx={{ width: 70, height: 70 }}
+            />
+          </Box>
+
+          {/* Center Logo */}
+          <Box
+            sx={{
+              position: "absolute",
+              top: "50%",
+              left: "50%",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              transform: animate
+                ? "translate(-50%, -50%) scale(1)"
+                : "translate(-50%, -50%) scale(0)",
+              transition: "transform 0.8s ease-in-out 0.4s",
+            }}
+          >
+            <img height={100} src={logo} />
+          </Box>
+        </Box>
       </WelcomeContainer>
     </Dialog>
   );
